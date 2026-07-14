63d6ad02d65fb08e4d603871209faa92ff46be6e
Muzaffar-0786 <mdmuzaffar88098@gmail.com>
2026-07-14 10:41:14 +0530

    Create dependencies.py for authentication handling
    Added authentication dependencies and user retrieval logic.

---
+++ dependencies.py
@@ -0,0 +1,48 @@
+"""
+Project : Real-Time AI Chat (MVP)
+File    : dependencies.py
+
+Authentication Dependencies
+"""
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from auth import decode_access_token
+from crud import get_user_by_id
+from database import get_db
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/login"
+)
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db),
+):
+
+    user_id = decode_access_token(token)
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token.",
+        )
+
+    user = get_user_by_id(db, user_id)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found.",
+        )
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Inactive account.",
+        )
+
+    return user

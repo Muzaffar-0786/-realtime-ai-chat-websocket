6377d118fcc2e65c2b60dfc06e8887672263c94e
Muzaffar-0786 <mdmuzaffar88098@gmail.com>
2026-07-14 18:36:22 +0530

    Fix indentation for receiving JSON data

--- websocket.py
+++ websocket.py
@@ -110,8 +110,7 @@ async def websocket_chat(
             return
 
         while True:
-            data = await 
-        websocket.receive_json()
+            data = await websocket.receive_json()
 
             if not isinstance(data, dict):
 
